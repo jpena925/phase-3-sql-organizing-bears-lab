4883cf13ed5195f5f5b9e2ca87cc1c7e515220cb
Jack Pena <penajackc@gmail.com>
2022-02-24 15:06:26 -0700

    commit tree db3bead4cb466d60e4cb37c8b1cf1010da4bd290

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", true);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", true);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", false);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", true);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", true);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", true);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", false);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", false);
